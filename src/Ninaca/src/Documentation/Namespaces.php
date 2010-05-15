8806c4047c8251d6602d4ba23df3c89f7d7c4f28
Nivl <nivl@free.fr>
2010-05-15 23:39:53 +0200

    Edit documentation

--- src/Ninaca/src/Documentation/Namespaces.php
+++ src/Ninaca/src/Documentation/Namespaces.php
@@ -1,39 +1,44 @@
 <?php
 
 /*!
-** @namespace Ninaca
+** \file Namespace.php
+**
+** This file is used to build the documentation with doxygen.
+*/
+
+
+/*!
+** \namespace Ninaca
 ** This namespace is the main namespace of the project.
 */
 
 
 /*!
-** @namespace Ninaca::Utilities
+** \namespace Ninaca::Utilities
 ** This namespace contains all classes with only static methods.
 */
 
 
 /*!
-** @namespace Ninaca::Parsers
+** \namespace Ninaca::Parsers
 ** This namespace is the main namespace for all parsers.
 */
 
 
 /*!
-** @namespace Ninaca::Parsers::Yaml
+** \namespace Ninaca::Parsers::Yaml
 ** This namespace contains all classes for dealing with YAML files.
 */
 
 
 /*!
-** @namespace Ninaca::Caches
+** \namespace Ninaca::Caches
 ** This namespace contains all classes for dealing with caching systems.
 */
 
 
 /*!
-** @namespace Ninaca::Exceptions
+** \namespace Ninaca::Exceptions
 ** This namespace contains all classes to throw exceptions.
 */
 
-
-?>
